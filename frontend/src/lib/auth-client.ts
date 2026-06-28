53fa598a5efdc264ea861d659e07df3640ed1dc7
d-brightmind <robiuridwanullah123@gmail.com>
2026-06-28 19:57:44 +0100

    Fixing Vercel deploy issues

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { polarClient } from "@polar-sh/better-auth/client"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+    baseURL: process.env.BETTER_AUTH_URL || "https://ai-voice-studio-app-alpha.vercel.app",
     /** The secret used to sign the tokens (optional if your server is configured to not require it) */
     secret: process.env.BETTER_AUTH_SECRET || undefined,
     plugins: [polarClient()]
